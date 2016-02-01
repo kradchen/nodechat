0e542fa97268f803817e424e7361d3f17a938159
kradchen <reinhard1203@163.com>
2016-02-01 16:52:22 +0800

    2月1日下午，第一次修改签入

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login');
 });
-
+router.post('/', function(req, res, next) {
+  console.log(req.body.UserID);
+  console.log(req.body.Pwd);
+});
 module.exports = router;
