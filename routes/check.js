8449720343af6be0ae7f3bafc0d04d928e906151
kradchen <reinhard1203@163.com>
2016-02-15 12:17:10 +0800

    2月15日上午，基本完成

--- routes/check.js
+++ routes/check.js
@@ -3,64 +3,55 @@
  */
 var express = require('express');
 var router = express.Router();
-var http = require('http');
+var request = require('request');
 var low = require('lowdb');
-var storage = require('lowdb/file-async');
-var db = low('/db.json', { storage });
-function Communicate(dre)
-{
-    var options;
-    var names = [
-        {Name:"330106196802020028",Pwd:"123456"},
-        {Name:"330106197112280062",Pwd:"123456"},
-        {Name:"330125197812250728",Pwd:"123456"},
-        {Name:"330125197311082551",Pwd:"123456"},
-        {Name:"330125197704200037",Pwd:"123456"},
-        {Name:"330106198412035215",Pwd:"654321"}];
-    if(dre =='in'){
-        options =
-        {
-            hostname : '218.108.75.59',
-            port : 88,
-            method : 'POST',
-            path : '/cl/login/'
-        };
+var storage = require('lowdb/file-sync');
+var db = low('db.json', { storage });
+function Communicate(dre) {
+    var usernames = "330106196802020028;330106197112280062;330125197812250728;330125197311082551;330125197704200037;330106198412035215";
+    var pwd="123456;123456;123456;123456;123456;654321";
+    if (dre == 'in') {
+        request.post("http://218.108.75.59:88/cl/login/",
+            {form: {userNames: usernames, pwds:pwd}},
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log('STATUS: ' + response.statusCode);
+                    console.log('HEADERS: ' + JSON.stringify(response.headers));
+                    console.log('BODY: ' + body);
+                    db('ckMessages').remove();
+                    db('ckMessages').push(JSON.parse(body));
+                }
+            }
+        );
     }
-    else if(dre =='out'){
-        options =
-        {
-            hostname : '218.108.75.59',
-            port : 80,
-            method : 'POST',
-            path : '/login/login/',
-            header:{}
-        };
+    else if (dre == 'out') {
+        request.post("http://218.108.75.59:88/cl/logout/",
+            {form: {userNames: usernames, pwds:pwd}},
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log('STATUS: ' + response.statusCode);
+                    console.log('HEADERS: ' + JSON.stringify(response.headers));
+                    console.log('BODY: ' + body);
+                    db('ckMessages').remove();
+                    db('ckMessages').push(JSON.parse(body));
+                }
+            }
+        );
     }
-    else{
-        options =
-        {
-            hostname : '218.108.75.59',
-            port : 88,
-            method : 'POST',
-            path : '/cl/testconn/trans/'
-        };
+    else {
+        request.post("http://218.108.75.59:88/cl/testconn/trans/",
+            {form: {"usermsgs": usermsgs}},
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log('STATUS: ' + response.statusCode);
+                    console.log('HEADERS: ' + JSON.stringify(response.headers));
+                    console.log('BODY: ' + body);
+                    db('ckMessages').remove();
+                    db('ckMessages').push(body);
+                }
+            }
+        );
     }
-    console.log('options: ' + JSON.stringify(options));
-    var req = http.request(options,function(res){
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-        res.on('data',function (chunk) {
-            console.log('BODY: ' + chunk);
-            db('ckMessages').push(chunk);
-        });
-        res.on("end",function(res,pres){
-        });
-    });
-
-    var data = {tranStr:"hello world"};
-    req.write(require('querystring').stringify(names));
-    req.end();
 }
 router.get('/', function(req, res) {
     if(req.cookies.loginFlag=='1') {
@@ -73,17 +64,29 @@ router.get('/', function(req, res) {
 router.post('/in/',function(req, res) {
     if(req.cookies.loginFlag=='1') {
         Communicate('in');
-        return ;
+        res.json({StatusCode:200,ResultMessage:"申请已提交"});
     }
-    res.redirect('/');
+    else {
+        res.redirect('/');
+    }
 });
 router.post('/out/',function(req, res) {
     if(req.cookies.loginFlag=='1') {
-        var myArray=new Array({StatusCode:'888',ResultMessage:"Success out!"});
-        res.json(myArray);
+        Communicate('in');
+        res.json({StatusCode:200,ResultMessage:"申请已提交"});
+    }
+    else {
+        res.redirect('/');
+    }
+});
+
+router.get('/msg/',function(req,res){
+    if(req.cookies.loginFlag=='1') {
+        var tableCk = db('ckMessages');
+        res.json(tableCk);
+        tableCk.remove();
         return ;
     }
     res.redirect('/');
 });
-
 module.exports = router;
